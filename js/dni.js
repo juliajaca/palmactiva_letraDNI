2e332218020bd0a90ad548bdbb11f68dbe41008d
juliajaca <juliajaca@hotmail.com>
2020-05-25 22:17:27 +0200

    añadido archivo con la funciones modulares de averiguar la letra del DNI

---
+++ js/dni.js
@@ -0,0 +1,40 @@
+const LETRAS_DNI = [
+    "T",
+    "R",
+    "W",
+    "A",
+    "G",
+    "M",
+    "Y",
+    "F",
+    "P",
+    "D",
+    "X",
+    "B",
+    "N",
+    "J",
+    "Z",
+    "S",
+    "Q",
+    "V",
+    "H",
+    "L",
+    "C",
+    "K",
+    "E"
+];
+
+function averiguarLetra(numeroDni){
+    var resto = numeroDni % 23;
+    return LETRAS_DNI[resto]
+}
+
+function imprimir(numeroDNI){
+    var letra = averiguarLetra(numeroDNI);
+    document.write(`El DNI completo es ${numeroDNI}-${letra}.`);
+    console.log(`El DNI completo es ${numeroDNI}-${letra}.`);
+}
+
+function dniCompleto(numeroDni){
+    imprimir(numeroDni);
+}
